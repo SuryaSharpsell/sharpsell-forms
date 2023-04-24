8d6c1f403189ddeac77742508e9b9376120bd4fc
Aman <aman.saxena@enparadigm.com>
2023-04-24 19:45:10 +0530

    on-site changes

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html.original": "4dea3e15d8e3d6d0e4f854cef8d5fcaf",
+  "index.html.original": "791228eb909cb9d8546f122ddc244223",
 "version.json": "6cf8b05506c81dcd40c2e2ea0864ba60",
 "firebase-config-web.js": "54b3e06e180fce0d4bb230c048c48b4d",
-"index.html": "1fa6fd6b4faa473086767805ada2b4a2",
-"/": "1fa6fd6b4faa473086767805ada2b4a2",
+"index.html": "671e6e399c40bc47e2cd2582141bf718",
+"/": "671e6e399c40bc47e2cd2582141bf718",
 "app_icon.png": "ef52c43fbc9d09a682e05b1d2883db64",
 "main.dart.js": "e0a9f3b2924313f333bdabf0114b886c",
 "firebase-app.js": "58a198434f551fd90f9f355acc4cfd1f",
